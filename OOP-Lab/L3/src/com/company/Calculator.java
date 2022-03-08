2b6eb8f585935cc09a55d612907e262e7a8ad840
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2022-03-08 17:43:10 +0500

    code cleanup

--- OOP-Lab/L3/src/com/company/Calculator.java
+++ OOP-Lab/L3/src/com/company/Calculator.java
@@ -2,13 +2,6 @@ package com.company;
 
 public class Calculator {
 
-    int a, b;
-
-    public int add() {
-        subtract(4, 2);
-        return a + b;
-    }
-
     public static void add(int x, int y) {
         System.out.println("Addition  " + x + " + " + y + " = " + (x + y));
     }
