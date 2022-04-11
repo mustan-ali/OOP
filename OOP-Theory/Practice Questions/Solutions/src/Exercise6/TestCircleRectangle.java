a58f9cc8f53be91a8c667bb0360b634f1efb88aa
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2022-04-11 21:22:34 +0500

    code cleanup

--- OOP-Theory/Practice Questions/Solutions/src/Exercise6/TestCircleRectangle.java
+++ OOP-Theory/Practice Questions/Solutions/src/Exercise6/TestCircleRectangle.java
@@ -2,14 +2,13 @@ package Exercise6;
 
 public class TestCircleRectangle {
 
-    public void Circle() {
+    public static void main(String[] args) {
+
         CircleFromGeometricObject c = new CircleFromGeometricObject(1.3, "blue");
         System.out.println(c.toString());
-    }
 
-    public void Rectangle() {
         RectangleFromSimpleGeometricObject r = new RectangleFromSimpleGeometricObject(2.5, 3.5, "red");
         System.out.println(r.toString());
-    }
 
+    }
 }
