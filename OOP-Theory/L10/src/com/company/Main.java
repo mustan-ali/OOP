dfb81ec54e4083ada20c22cb8a0af4955595fae7
Mustan Ali <mustan9900@gmail.com>
2022-05-22 12:11:13 +0500

    added Access Modifier definition

--- OOP-Theory/L10/src/com/company/Main.java
+++ OOP-Theory/L10/src/com/company/Main.java
@@ -14,3 +14,15 @@ public class Main {
 
     }
 }
+
+//    Modifier	    Description
+//    Default	    declarations are visible only within the package (package private)
+//    Private	    declarations are visible within the class only
+//    Protected	    declarations are visible within the package or all subclasses
+//    Public	    declarations are visible everywhere
+
+//    AccessModifier--> within class - within package - outside package by subclass only + outside package
+//    Private-->Y-N-N-N
+//    Default-->Y-Y-N-N
+//    Protected-->Y-Y-Y-N
+//    Public-->Y-Y-Y-Y
