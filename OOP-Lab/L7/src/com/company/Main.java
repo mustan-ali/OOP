29a1ece7f40078e68fb362382790543d305cf5d5
Mustan Ali <mustan9900@gmail.com>
2022-06-09 16:52:37 +0500

    uncomment code

--- OOP-Lab/L7/src/com/company/Main.java
+++ OOP-Lab/L7/src/com/company/Main.java
@@ -4,18 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Point_2D p2D = new Point_2D();
-//
-//        System.out.println(p2D);    //Print value from default constructor
-//
-//        Point_2D p2D1 = new Point_2D(4,6);
-//        System.out.println(p2D1);
-//
-//        Point_3D p3D = new Point_3D();
-//        System.out.println(p3D);        //Print value from default constructor
-//
-//        Point_3D p3D1 = new Point_3D(4,6,7);
-//        System.out.println(p3D1);
+        Point_2D p2D = new Point_2D();
+
+        System.out.println(p2D);    //Print value from default constructor
+
+        Point_2D p2D1 = new Point_2D(4,6);
+        System.out.println(p2D1);
+
+        Point_3D p3D = new Point_3D();
+        System.out.println(p3D);        //Print value from default constructor
+
+        Point_3D p3D1 = new Point_3D(4,6,7);
+        System.out.println(p3D1);
 
         System.out.println("-----------------------------------------");
 
